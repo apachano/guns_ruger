241b61611db5dd25a192d4a9536e8aff44e7edea
Austin <pachano.austin@gmail.com>
2018-03-13 04:39:02 -0700

    Initial commit

---
+++ init.lua
@@ -0,0 +1,31 @@
+local path = minetest.get_modpath("guns_ruger")
+
+local entity = {}
+	entity.visual = "sprite"
+	entity.visual_size = {x = 0.5, y = 0.5}
+	entity.textures = "22.png"
+	entity.hit_player = function(self, player)
+		player:punch(self.object, 1.0, {
+			full_punch_interval = 1.0,
+			damage_groups = {fleshy = 2},
+		}, nil)
+	end
+	entity.hit_node = function(self, pos, node)
+	end
+
+
+guns.register_ammo("guns_ruger",{
+		name = "22cal",
+		max_ammo = 100,
+		damage = 1,
+		entity = entity,
+		inventory_image = "22.png"
+	})
+
+guns.register_gun("guns_ruger",{
+		name = "ruger",
+		ammo = "guns_ruger:22cal",
+		inventory_image = "ruger.png",
+		sound_fire = "ruger_fire",
+		sound_empty = "ruger_empty",
+	})
---
+++ readme.md
@@ -0,0 +1,16 @@
+# guns_ruger
+Author: Austin Pachano 
+Version: 0.1
+This is an example on how to use guns framework to register a new gun
+
+## Credit
+
+### Textures
+ruger.png 		SonosFuer
+
+### Sounds
+ruger_empty.ogg http://www.bigsoundbank.com/sound-0029-computer-mouse.html
+ruger_fire.ogg 	http://www.bigsoundbank.com/sound-0438-shot-in-357-magnum-9-mm.html
+
+## Change Log
+0.1 - Initial Upload
